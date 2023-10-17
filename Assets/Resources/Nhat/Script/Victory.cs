963d2a809d5b307d54c98bd4aea79f121f46a1fe
HNLNhat <nhathnlps22039@fpt.edu.vn>
2023-10-17 09:47:00 +0700

    Nhat
    Panel victory

--- Assets/Resources/Nhat/Script/Victory.cs
+++ Assets/Resources/Nhat/Script/Victory.cs
@@ -1,25 +1,47 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Victory : MonoBehaviour
 {
     public GameObject victoryPanel;
     private bool isGamePaused = false;
-  
     public Button restartButton;
     public Button continueButton;
+
     private void Start()
     {
-        victoryPanel.SetActive(false); // Ẩn victory panel ban đầu
+        ResumeGame(); // Bắt đầu game chưa tạm dừng.
+
+        // Gắn các hàm xử lý cho các nút tương ứng.
+        restartButton.onClick.AddListener(RestartGame);
+        continueButton.onClick.AddListener(ResumeGame);
+    }
+    public void RestartGame()
+    {
+        // Gọi hàm này khi bạn muốn khởi đầu lại trò chơi sau khi nhân vật chết.
+        Time.timeScale = 1; // Đảm bảo rằng thời gian đang chạy bình thường.
+        isGamePaused = false;
+        victoryPanel.SetActive(false); // Ẩn Panel Pause.
+
+        // Điều hướng đến màn hình chơi lại (thay đổi tên scene nếu cần).
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1; // Khôi phục thời gian về bình thường.
+        isGamePaused = false;
+        victoryPanel.SetActive(false); // Ẩn Panel Pause.
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player")) // Kiểm tra xem đối tượng va chạm có tag "Player" hay không
         {
             Time.timeScale = 0; // Tạm dừng thời gian trong trò chơi.
             isGamePaused = true;
-            victoryPanel.SetActive(true); // Hiển thị victory panel
+            victoryPanel.SetActive(true); // Hiển thị Panel Pause.
         }
     }
+
 }
