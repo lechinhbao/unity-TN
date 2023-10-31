515eeeffd8cfc5d8a3293e5d7b613d1655648ca0
HNLNhat <nhathnlps22039@fpt.edu.vn>
2023-10-31 12:34:08 +0700

    Nhat
    Screeen

--- Assets/Resources/Nhat/Script/PlayerHealth.cs
+++ Assets/Resources/Nhat/Script/PlayerHealth.cs
@@ -77,7 +77,6 @@ public class PlayerHealth : MonoBehaviour
     {
 
     }
-    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Monster"))
