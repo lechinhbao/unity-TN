bc46e9990ed2ea2d078237ce944c417c814e6069
thanhhkhoi <khoinguyen2003hg@gmail.com>
2023-11-06 14:00:41 +0700

    6/11/2pm

--- Assets/Resources/Khoi/Script_K/Player.cs
+++ Assets/Resources/Khoi/Script_K/Player.cs
@@ -5,14 +5,52 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
+    private new Rigidbody2D rigidbody2D;
+    private bool isNen;
+    private Animator animator;
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-   
+        Vector2 scale = transform.localScale;
+        animator.SetTrigger("isRunning");
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetTrigger("isRunning");
+            scale.x = 1;
+            transform.Translate(Vector3.right * 5 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            animator.SetTrigger("isRunning");
+            scale.x = -1;
+            transform.Translate(Vector3.left * 5 * Time.deltaTime);
+        }
+
+        transform.localScale = scale;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (isNen)
+            {
+                rigidbody2D.AddForce(new Vector2(0, 300));
+                isNen = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Nen")
+        {
+            isNen = true;
+        }
     }
 }
