2089b18dd255d54d33848e05fbdbebcbc1e93d3b
HNLNhat <nhathnlps22039@fpt.edu.vn>
2023-11-17 20:52:17 +0700

    Nhat
    17/11

---
+++ Assets/Resources/Nhat/Script/Panel/Star.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Star : MonoBehaviour
+{
+    public List<TMP_Text> coinTexts;
+
+    private int coinsCollected = 0;      // Số xu đã ăn
+    public TMP_Text coinText;               // Text để hiển thị số xu
+    public TMP_Text coinTextPanelDie;               // Text để hiển thị số xu
+
+    public GameObject[] stars;          // Mảng chứa các sao (đặt trong Inspector)
+    public int[] starThresholds;         // Ngưỡng số xu để bật mỗi sao
+
+    private int star = 0;            // Số sao
+    public TMP_Text starText;            // Text để hiển thị số sao
+    public TMP_Text starTexPanelDie;            // Text để hiển thị số sao
+    void Start()
+    {
+        UpdateCoins();  // Cập nhật số xu khi bắt đầu
+        ActivateStars();  // Kích hoạt sao dựa trên số xu đã ăn được
+    }
+
+    void UpdateCoins()
+    {
+        coinText.text = "Coins: " + coinsCollected.ToString();  // Cập nhật số xu trong Text
+        starText.text = "Stars: " + star.ToString();           // Cập nhật số sao trong Text
+    }
+
+    void ActivateStars()
+    {
+        for (int i = 0; i < stars.Length; i++)
+        {
+            if (star >= starThresholds[i])
+            {
+                stars[i].SetActive(true);  // Kích hoạt sao nếu số xu đạt ngưỡng
+            }
+        }
+    }
+    // Hàm này có thể được gọi khi nhân vật ăn một số xu (tùy thuộc vào game logic)
+/*    public void CollectCoin(int amount)
+    {
+        coinsCollected += amount;  // Tăng số xu đã ăn
+        UpdateCoins();            // Cập nhật số xu
+        ActivateStars();          // Kích hoạt sao tương ứng
+    }*/
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            coinsCollected += 1;
+            coinText.text = coinsCollected + "Score:";
+            Destroy(collision.gameObject);
+            star += 10;              // Cộng 10 sao mỗi khi ăn 1 xu
+            UpdateCoins();            // Cập nhật số xu
+            ActivateStars();          // Kích hoạt sao tương ứng
+        }
+    }
+}
